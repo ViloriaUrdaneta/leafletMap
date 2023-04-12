3acc0792f91bcc2cb9d9957a9069295ea4d8d9b4
Miguel <miguel.viloria.247@gmail.com>
2023-04-12 17:58:29 -0400

    pointer

--- src/components/IconLocation.js
+++ src/components/IconLocation.js
@@ -1,7 +1,8 @@
 import L from 'leaflet';
+import pointer from '../assets/icon.svg';
 
 export const IconLocation = L.icon({
-    iconUrl: require('../assets/icon.svg'),
+    iconUrl: pointer,
     iconRetinaUrl: require('../assets/icon.svg'),
     iconSize: [35, 35],
     className: 'leaflet-venue-icon'
--- src/components/Markers.js
+++ src/components/Markers.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Marker } from 'react-leaflet';
-import { IconLocation } from './IconLocation';
+//import { IconLocation } from './IconLocation';
+import { Icon } from 'leaflet';
+import pointer from '../assets/icon.svg'
 
 const Markers = () => {
+
+    const customIcon = new Icon({
+        iconUrl: pointer,
+        iconSize: [35, 35],
+    })
+
     return (
         <Marker
             position={{lat: '51.528845', lng: '-0.172728'}}
-            icon={IconLocation}
+            icon={customIcon}
         ></Marker>
     );
 }
